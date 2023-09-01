0b8da272d8a04724ff72baef278cd9807a063d65
Michael Kim <michael.kim.200518@gmail.com>
2023-09-01 14:44:41 +0900

    Add del-validator

--- agora/cmd.sh
+++ agora/cmd.sh
@@ -148,6 +148,14 @@ elif [ "$1" = "stop-relay" ]; then
 
   docker-compose -f "$agora_root"/relay/docker-compose.yml down
 
+elif [ "$1" = "start-del-validators" ]; then
+
+  docker-compose -f "$agora_root"/del-validators/docker-compose.yml up -d
+
+elif [ "$1" = "stop-del-validators" ]; then
+
+  docker-compose -f "$agora_root"/del-validators/docker-compose.yml down
+
 elif [ "$1" = "start-graph" ]; then
 
   docker-compose -f "$agora_root"/graph/docker-compose.yml up -d
